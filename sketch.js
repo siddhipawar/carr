c33f037a9597f153618fff2224dedcf76a872617
siddhipawar <73481369+siddhipawar@users.noreply.github.com>
2020-12-15 14:19:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,7 +30,43 @@ function setup() {
 
 function draw() {
   background(0);
-wall.display();
-bullet.display();
+ if(hasCollided(bullet, wall))
+  {
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+
+  	
+	if(damage>10)
+	{
+		wall.shapeColor=color(255,0,0);
+		
+	}
+
+	
+
+	if(damage<10)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+	
+  }
+
+
+  drawSprites();
+ 
+}
+
+
+function hasCollided(lbullet, lwall)
+{
+	bulletRightEdge=lbullet.x +lbullet.width;
+	wallLeftEdge=lwall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}
+	return false;
+}
+
 
 }
